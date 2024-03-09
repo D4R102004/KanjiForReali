cd022e11b0265e40bb1181f0fb827b1633f03100
D4R102004 <161531325+D4R102004@users.noreply.github.com>
2024-03-09 12:43:18 -0700

    we can draw

---
+++ KanjiUnity/Assets/Scripts/DrawCard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawCard : MonoBehaviour 
+{
+	public GameObject Card1;
+	public GameObject PlayerHand;
+	List<GameObject> cards = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		cards.Add(Card1);
+	}
+	public void Draw()
+	{
+		GameObject playercard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0,0,0), Quaternion.identity);
+		playercard.transform.SetParent(PlayerHand.transform);
+	}
+
+	
+}
