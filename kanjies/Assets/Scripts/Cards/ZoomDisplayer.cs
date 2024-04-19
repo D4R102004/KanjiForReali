25e78248929c7d311eb83462ac739077b786a494
D4R102004 <161531325+D4R102004@users.noreply.github.com>
2024-04-19 12:42:02 -0400

    Same game but prettier
    Same game but prettier

--- kanjies/Assets/Scripts/Cards/ZoomDisplayer.cs
+++ kanjies/Assets/Scripts/Cards/ZoomDisplayer.cs
@@ -7,9 +7,9 @@ public class ZoomDisplayer : MonoBehaviour {
 	public void GetZoom(Component sender, object data1, object data2, object data3)
 	{
 		GameObject card = (GameObject)data1;
+        card.transform.SetParent(this.gameObject.transform, true);
+        card.transform.position = this.transform.position;
         RectTransform rect = card.GetComponent<RectTransform>();
         rect.sizeDelta = new Vector2(209, 218);
-        card.transform.SetParent(this.gameObject.transform);
-        card.transform.position = this.transform.position;
 }
 }
